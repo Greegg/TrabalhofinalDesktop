b8d35e7f1cc6344b556943d6f352bd1816587dea
@Greegg <>
2018-11-15 22:47:33 -0200

    Versão Alpha trabalho final prog. desktop

---
+++ src/Anotacoes/Anotacoes.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Anotacoes;
+
+/**
+ *
+ * @author Alunos
+ */
+public class Anotacoes {
+    
+    /**
+     * CRIAR MODELOS
+     * CRIAR CONEXAO BANCO
+     * CRIAR DAO
+     * CRIAR CONTROL
+     * CRIAR VIEW
+     * 
+     * 
+     * 
+     * 
+     * 
+     * 
+     * 
+     * 
+     * 
+     * criar ativos nas tabelas cliente, categoria, produto, encomenda
+     * 
+     */
+    
+}
---
+++ src/sc/senac/br/modelo/EncomendaProduto.java
@@ -0,0 +1,73 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sc.senac.br.modelo;
+
+/**
+ *
+ * @author Alunos
+ */
+public class EncomendaProduto {
+    
+    private int id;
+    private Encomenda encomenda;
+    private Produto produto;
+    private int quantidade;
+    private float valor;
+
+    public EncomendaProduto() {
+    }
+
+    public EncomendaProduto(int id, Encomenda encomenda, Produto produto, int quantidade, float valor) {
+        this.id = id;
+        this.encomenda = encomenda;
+        this.produto = produto;
+        this.quantidade = quantidade;
+        this.valor = valor;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Encomenda getEncomenda() {
+        return encomenda;
+    }
+
+    public void setEncomenda(Encomenda encomenda) {
+        this.encomenda = encomenda;
+    }
+
+    public Produto getProduto() {
+        return produto;
+    }
+
+    public void setProduto(Produto produto) {
+        this.produto = produto;
+    }
+
+    public int getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(int quantidade) {
+        this.quantidade = quantidade;
+    }
+
+    public float getValor() {
+        return valor;
+    }
+
+    public void setValor(float valor) {
+        this.valor = valor;
+    }
+    
+    
+    
+}
