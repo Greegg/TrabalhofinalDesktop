82f73b3407c266c9c8a04156fd7f62c505678161
@Greegg <hvb@192.168.15.3>
2018-11-19 11:06:15 -0200

    Versão Alpha trabalho final prog. desktop 0.2(Dao Cliente)

--- src/sc/senac/br/modelo/Cliente.java
+++ src/sc/senac/br/modelo/Cliente.java
@@ -5,7 +5,7 @@
  */
 package sc.senac.br.modelo;
 
-import java.util.Date;
+import java.sql.Date;
 
 /**
  *
--- src/sc/senac/br/modelo/dao/ClienteDao.java
+++ src/sc/senac/br/modelo/dao/ClienteDao.java
@@ -6,6 +6,8 @@
 package sc.senac.br.modelo.dao;
 
 import java.sql.Date;
+import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 import sc.senac.br.interfaces.DaoI;
 import sc.senac.br.modelo.Cliente;
@@ -36,27 +38,108 @@ public class ClienteDao extends Dao implements DaoI<Cliente> {
 
     @Override
     public boolean atualizar(Cliente obj) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        try {
+            stmt = conexao.prepareStatement("UPDATE cliente SET nome = ?, data_nascimento=?, email=?, telefone=? WHERE id=?");
+
+            stmt.setString(1, obj.getNome());
+            stmt.setDate(2, obj.getDataNascimento());
+            stmt.setString(3, obj.getEmail());
+            stmt.setString(4, obj.getTelefone());
+            stmt.setInt(4, obj.getId());
+
+            if (stmt.executeUpdate() > 0) {
+                return true;
+            } else {
+                return false;
+            }
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            return false;
+        }
     }
 
     @Override
     public boolean excluir(Cliente obj) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        try {
+            stmt = conexao.prepareStatement("UPDATE cliente SET status = 0 WHERE id= ?");
+            stmt.setInt(1, obj.getId());
+            return (stmt.executeUpdate() > 0);
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+            return false;
+        }
     }
 
     @Override
     public List<Cliente> listar() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        try {
+            this.sql = "SELECT id, nome, data_nascimento, email, telefone FROM cliente WHERE status = 1 ORDER BY id ASC";
+            stmt = conexao.prepareStatement(sql);
+            res = stmt.executeQuery();
+
+            List<Cliente> lista = new ArrayList<>();
+            while (res.next()) {
+                cliente = new Cliente();
+
+                cliente.setId(res.getInt("id"));
+                cliente.setNome(res.getString("nome"));
+                cliente.setDataNascimento(res.getDate("data_nascimento"));
+                cliente.setEmail(res.getString("email"));
+                cliente.setTelefone(res.getString("telefone"));
+
+                lista.add(cliente);
+            }
+            return lista;
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            return null;
+        }
     }
 
     @Override
     public Cliente lerPorId(int id) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        try {
+            stmt = conexao.prepareStatement("SELECT id, nome, data_nascimento, email, telefone FROM cliente"); //seleciona o cliente pelo ID
+            stmt.setInt(1, id);
+            res = stmt.executeQuery();
+            
+            if (res.next()) { 
+                cliente.setId(res.getInt("id"));
+                cliente.setNome(res.getString("nome"));
+                cliente.setNome(res.getString("nome"));
+                cliente.setDataNascimento(res.getDate("data_nascimento"));
+                cliente.setEmail(res.getString("email"));
+                cliente.setTelefone(res.getString("telefone"));
+            }
+            
+            return cliente;
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+            return null;
+        }
     }
 
     @Override
     public List<Cliente> pesquisarNome(String nome) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        try {
+            stmt = conexao.prepareStatement("SELECT id, nome, dataNascimento, cpf FROM cliente WHERE nome LIKE?");
+            stmt.setString(1, nome+"%");
+            res = stmt.executeQuery();
+            List<Cliente> lista = new ArrayList<>();
+            while (res.next()) {
+                cliente.setId(res.getInt("id"));
+                cliente.setNome(res.getString("nome"));
+                cliente.setNome(res.getString("nome"));
+                cliente.setDataNascimento(res.getDate("data_nascimento"));
+                cliente.setEmail(res.getString("email"));
+                cliente.setTelefone(res.getString("telefone"));
+                lista.add(cliente);
+            }
+            return lista;
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+            return null;
+        }
     }
 
 }
